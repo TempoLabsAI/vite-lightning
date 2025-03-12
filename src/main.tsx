296156093a56ea7a01251373b9111964c2d039f8
fehervaryy <martindmhe@gmail.com>
2025-03-12 19:38:04 -0400

    use salesforce lightning design system and lwc

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import '@salesforce-ux/design-system/assets/styles/salesforce-lightning-design-system.min.css';
 
 import { TempoDevtools } from "tempo-devtools";
 TempoDevtools.init();
