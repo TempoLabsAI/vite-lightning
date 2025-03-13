b0ac02fae57d49df6d2cc88065820d55b4f5c460
fehervaryy <martindmhe@gmail.com>
2025-03-13 11:56:53 -0400

    temp remove alias

--- vite.config.ts
+++ vite.config.ts
@@ -26,7 +26,7 @@ export default defineConfig({
     preserveSymlinks: true,
     alias: {
       "@": path.resolve(__dirname, "./src"),
-      "/assets": "node_modules/@salesforce-ux/design-system/assets",
+      // "/assets": "node_modules/@salesforce-ux/design-system/assets",
     },
   },
   server: {
